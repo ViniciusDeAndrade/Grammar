8aef985c3929018c29f43d6cb5b3d4f6a642bc78
Vinicius <mvaa@cin.ufpe.br>
2017-06-01 11:46:53 -0300

    package change

--- Project/src/control/Manager.java
+++ Project/src/control/Manager.java
@@ -1,6 +1,8 @@
-package entidades;
+package control;
 
 import java.util.List;
+
+import entity.Connector;
 
 public class Manager {
 
--- Project/src/entity/Connector.java
+++ Project/src/entity/Connector.java
@@ -1,4 +1,4 @@
-package entidades;
+package entity;
 
 public class Connector {
 
